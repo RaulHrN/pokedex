f4aadbfcd4041ae45a6e3fe0f4d7e158759d75fa
Raul Nascimento <raul.hnr23@gmail.com>
2022-08-31 18:56:52 -0300

    cc

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Axios, AxiosResponse } from "axios";
 import React, { useEffect, useState } from "react";
-import { Pokemon } from "./models/pokemon";
+import { Pokemon, PokemonTypeColor } from "./models/pokemon";
 import { Response, ResponsePokemon } from "./models/response";
 import { pokedex } from "./services/pokedex";
 
@@ -15,25 +15,45 @@ function App() {
     pokedex
       .get("https://pokeapi.co/api/v2/pokemon")
       .then((response: AxiosResponse<ResponsePokemon, any>) => {
-        //setPokemons(response.data.results);
-        response.data.results.forEach((pokemon) => getPokemonData(pokemon.url))
+        setPokemons(response.data.results);
+        response.data.results.forEach((pokemon) =>
+          getPokemonData(pokemon.url, pokemon.name)
+        );
       })
       .catch((err) => {
         console.error("O pokemon não existe: " + err);
       });
   };
 
-  const getPokemonData = (url: string): void => {
-    pokedex.get(url).then((response: AxiosResponse<any, any>) => {
-      console.log(response)
-    })
-  }
+  const getPokemonData = (url: string, name: string): void => {
+    pokedex.get(url).then((response: AxiosResponse<Pokemon, any>) => {
+      setPokemons((pokemonList) =>
+        pokemonList.map((pokemon) => {
+          if (name === pokemon.name) {
+            return { ...response.data, ...pokemon };
+          }
+          return pokemon;
+        })
+      );
+    });
+  };
 
   return (
     <div className="App">
       <div>
-        {pokemons.map((pokemon) => {
-          return <div>{pokemon.name}</div>;
+        {pokemons.map((pokemon, index) => {
+          return (
+            <div key={index}>
+              <p>{pokemon.name}</p>
+              <p>{pokemon.id}</p>
+              <img src={pokemon.sprites?.front_default} alt={pokemon.name}/>
+              {pokemon.types&&pokemon.types.map((type) => {
+                return (
+                  <p style={{backgroundColor: PokemonTypeColor[type.type.name]}}>{type.type.name}</p>
+                )
+              })}
+            </div>
+          );
         })}
       </div>
     </div>
@@ -44,4 +64,4 @@ export default App;
 
 /* const {data}: ResponsePokemon = response 
 
-COLOCAR NO SETPOKEMON TODOS OS POKEMONS QUE RETORNAREM NO RESPONSE*/
+TRANSFORMAR DIV PAI EM NOVO COMPONENT (POKEMON_CARD)*/
--- src/models/pokemon.ts
+++ src/models/pokemon.ts
@@ -1,9 +1,41 @@
-export interface Pokemon{
-    name: string;
-    url: string;
-    abilities:
+export interface Pokemon {
+  name: string;
+  url: string;
+  height: number;
+  id: number;
+  sprites?: {
+    front_default: string;
+  };
+  types?: PokemonType[];
+  weight: number;
 }
 
-interface Abilities{
-    
+interface PokemonType{
+    slot: number;
+    type: {
+        name: PokemonTypes;
+    }
+}
+
+type PokemonTypes = "dark" | "dragon" | "electric" | "fairy" | "fighting" | "fire" | "flying" | "ghost" | "grass" | "ground" | "ice" | "bug" | "normal"  | "poison" | "psychic" | "rock" | "steel" |  "water";
+
+export enum PokemonTypeColor {
+  dark = "#707070",
+  dragon = "#f16e57",
+  electric = "#eed535",
+  fairy = "#fdb9e9",
+  fighting = "#d56723",
+  fire = "#fd7d24",
+  flying = "#3dc7ef",
+  ghost = "#7b62a3",
+  grass = "#9bcc50",
+  ground = "#f7de3f",
+  ice = "#51c4e7",
+  bug = "#729f3f",
+  normal = "#a4acaf",
+  poison = "#b97fc9",
+  psychic = "#f366b9",
+  rock = "#a38c21",
+  steel = "#9eb7b8",
+  water = "#4592c4"
 }
